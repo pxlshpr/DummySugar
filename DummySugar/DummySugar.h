958bd69aeed1b9e4378371cb90dfaead136e78c3
Ahmed Khalaf <pxlshpr@gmail.com>
2017-08-04 14:08:05 +0800

    initial commit

---
+++ DummySugar/DummySugar.h
@@ -0,0 +1,19 @@
+//
+//  DummySugar.h
+//  DummySugar
+//
+//  Created by pxlshpr on 4/8/17.
+//  Copyright © 2017 pxlshpr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DummySugar.
+FOUNDATION_EXPORT double DummySugarVersionNumber;
+
+//! Project version string for DummySugar.
+FOUNDATION_EXPORT const unsigned char DummySugarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DummySugar/PublicHeader.h>
+
+
---
+++ DummySugar/DummySugar.swift
@@ -0,0 +1,15 @@
+//
+//  DummySugar.swift
+//  DummySugar
+//
+//  Created by pxlshpr on 4/8/17.
+//  Copyright © 2017 pxlshpr. All rights reserved.
+//
+
+import Foundation
+
+public class DummySugar {
+  public class func hello() {
+    print("🍬 Hello from DummySugar")
+  }
+}
